a3dc5f983e053590e49b7cb64b14539bf2215fd8
Adam Johannesmeyer <adamj0101@gmail.com>
2017-01-24 19:02:24 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const DIV = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 60vw;
+`;
+
+const BUTTON = styled.button`
+  width: 50px;
+  height: 50px;
+  outline: none;
+  border: none;
+  &:hover {
+    background: #A4C175;
+    color: $fff;
+  }
+`
+const WRAPPER = styled.div`
+  display: flex; 
+  justify-content: space-around;
+  flex-direction: row;
+  width: 300px;
+`
+
+const COUNTER = styled.div`
+  font-size: 45px;
+  text-align: center;
+  width: 50px;
+`
+
+export class App extends React.Component<void, any> {
+  private counter: number;
+  constructor() {
+    super();
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+
+    this.state = { counter: 0};
+  }
+
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  decrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
+  render() {
+    return (
+      <DIV>
+        <WRAPPER>
+          <BUTTON onClick={() => this.decrement()}>
+          -
+          </BUTTON>
+          <COUNTER>{this.state.counter}</COUNTER>
+          <BUTTON onClick={() => this.increment()}>+</BUTTON>
+        </WRAPPER>
+      </DIV>  
+    );
+  }
+}
